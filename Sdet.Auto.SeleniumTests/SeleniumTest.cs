7c48bf6f7243de21ae8aedd5eac5a5fa7cd416af
sdetAutomation <sdet.testautomation@gmail.com>
2016-12-25 23:56:50 -0600

    init commit

---
+++ Sdet.Auto.SeleniumTests/SeleniumTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sdet.Auto.SeleniumTests
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        [TestMethod]
+        public void TC0001_SmokeTest()
+        {
+        }
+
+        [TestMethod]
+        public void TC0002_ForgetPasswordTest()
+        {
+        }
+
+        [TestMethod]
+        public void TC0003_FormAuthentication()
+        {
+        }
+
+        [TestMethod]
+        public void TC0004_FormAuthenticationBadInfo()
+        {
+        }
+    }
+}
