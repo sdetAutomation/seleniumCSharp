a4a3f6f2b6a237e4ba14dd1bbbf060800e3cacbc
sdetAutomation <sdet.testautomation@gmail.com>
2016-12-26 13:57:26 -0600

    Add navigation class with nav to AUT

---
+++ Sdet.Auto.PageObjects/Navigation.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using Sdet.Auto.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdet.Auto.PageObjects
+{
+    public class Navigation : WebDriverBase
+    {
+        public static void NavToWebPageUnderTest()
+        {
+            var navigation = Driver.Navigate();
+            navigation.GoToUrl(ConfigurationManager.AppSettings["webUrl"]);
+        }
+    }
+}
--- Sdet.Auto.SeleniumTests/SeleniumTest.cs
+++ Sdet.Auto.SeleniumTests/SeleniumTest.cs
@@ -13,7 +13,8 @@ namespace Sdet.Auto.SeleniumTests
         public void TC0001_SmokeTest()
         {
             GuiHelper.OpenWebBrowser();
-
+            Navigation.NavToWebPageUnderTest();
+            Thread.Sleep(3000);
             GuiHelper.CloseWebBrowser();
 
         }
@@ -32,5 +33,68 @@ namespace Sdet.Auto.SeleniumTests
         public void TC0004_FormAuthenticationBadInfo()
         {
         }
+
+    //    @Test
+    //public void TC0001_SmokeTest()
+    //    {
+
+    //        GuiHelper.OpenWebBrowser();
+    //        Navigation.NavToWebPageUnderTest();
+    //        HomePage.VerifyOnHomePage(testAssert);
+    //        GuiHelper.CloseWebBrowser();
+    //    }
+
+    //    @Test
+    //public void TC0002_ForgetPasswordTest()
+    //    {
+
+    //        final String email = "sdet.testautomation@gmail.com";
+    //        final String expectedMsg = "Your e-mail's been sent!";
+
+    //        GuiHelper.OpenWebBrowser();
+    //        Navigation.NavToWebPageUnderTest();
+    //        HomePage.ClickForgetPassword();
+    //        ForgetPasswordPage.EnterEmail(email);
+    //        ForgetPasswordPage.ClickRetrieveButton();
+    //        EmailSentPage.VerifyEmailSent(testAssert, expectedMsg);
+    //        GuiHelper.CloseWebBrowser();
+    //    }
+
+    //    @Test
+    //public void TC0003_FormAuthentication()
+    //    {
+
+    //        final String userId = "tomsmith";
+    //        final String password = "SuperSecretPassword!";
+    //        final String expectedLoginMsg = "You logged into a secure area!";
+    //        final String expectedLogoutMsg = "You logged out of the secure area!";
+
+    //        GuiHelper.OpenWebBrowser();
+    //        Navigation.NavToWebPageUnderTest();
+    //        HomePage.ClickFormAuthentication();
+    //        LoginPage.EnterCredentials(userId, password);
+    //        SecureAreaPage.VerifyMessage(testAssert, expectedLoginMsg);
+    //        SecureAreaPage.ClickLogoutButton();
+    //        LoginPage.VerifyMessage(testAssert, expectedLogoutMsg);
+    //        GuiHelper.CloseWebBrowser();
+    //    }
+
+    //    @Test
+    //public void TC0004_FormAuthenticationBadInfo()
+    //    {
+
+    //        final String userId = "sdetAutomatiom";
+    //        final String password = "pass@word";
+    //        final String expectedMsg = "Your username is invalid!";
+
+    //        GuiHelper.OpenWebBrowser();
+    //        Navigation.NavToWebPageUnderTest();
+    //        HomePage.ClickFormAuthentication();
+    //        LoginPage.EnterCredentials(userId, password);
+    //        LoginPage.VerifyMessage(testAssert, expectedMsg);
+    //        GuiHelper.CloseWebBrowser();
+    //    }
+
+
     }
 }
